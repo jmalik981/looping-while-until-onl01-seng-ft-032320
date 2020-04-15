4dd6491f24e691eacadc07216dccd53e3a54e403
Junaid Malik <jmalik981@gmail.com>
2020-04-15 03:15:10 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,8 +2,9 @@ def using_while
   levitation_force = 6
 	while levitation_force < 10 
 	levitation_force += 1 
-	puts ""
+	puts "levitation number is #{levitation_force}"
   #your code here
+end
 end
 
 
